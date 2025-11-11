ea366642c6c9445a8dcff162db19ebed3d4e096d
Fabian <149208389+fabianL1310@users.noreply.github.com>
2025-11-11 18:34:34 +0100

    Feature/tryimproving (#6)
    * solving some exersises
    
    * feat: solve tasks and add data
    
    * improving model saving
    
    * base model
    
    * image augmentation
    
    * adding history of last angles
    
    * adding 2nd linear layer
    
    * adding 2 more conv layer
    
    * reducing kernel size on conv layer 5
    
    * using only 2 linear layers again (-1 from before)
    
    * fixing runs naming
    
    * testing kernel size 5 on conv layer 5
    
    * updating prediction.py to use angle history
    
    * fixing prediction.py
    
    * using best model again
    
    ---------
    
    Co-authored-by: Nico Benninger <nico.benninger43@gmail.com>

--- 03_Homework/prediction.py
+++ 03_Homework/prediction.py
@@ -1,20 +1,28 @@
+from collections import deque
 from pathlib import Path
 from typing import Dict
 
+
+
 import numpy as np
-from numpy import ndarray
 import openvino as ov
+from numpy import ndarray
 from openvino import CompiledModel
 from PIL import Image
 
 # ---------------- Config ----------------
 SPEED = 75.0        # regular speed
-DRIVE_MODEL_NAME = 'unequaled-skink-546_New_v4_2_v3_e30.onnx'
+DRIVE_MODEL_NAME = "DriveModel_v1.onnx"
+MEM_SIZE = 12
 
+angle_history = deque(maxlen=MEM_SIZE)
 
 # ---------------- Load ----------------
 def load(model_dir: str) -> CompiledModel:
     """This functions gets called every time the side button on the remote is pressed in self-driving mode"""
+    global angle_history
+    angle_history = deque([0.0]*MEM_SIZE, maxlen=MEM_SIZE)
+
     model_dir = Path(model_dir)
 
     drive_model_path = model_dir / DRIVE_MODEL_NAME
@@ -30,17 +38,32 @@ def load(model_dir: str) -> CompiledModel:
 # ---------------- Step ----------------
 def step(img, model) -> tuple[float, float, Dict[str, float]]:
     """This function gets called for every image from the cars camera"""
+    global angle_history
+
     drive_model = model
     drive_image, _ = img_to_tensor(img)
-    angle = predict_angle(drive_model, drive_image)
+
+    angle_history_list = list(reversed(angle_history))
+    if len(angle_history_list) < MEM_SIZE:
+        angle_history_list += [0.0] * (MEM_SIZE - len(angle_history_list))
+    else:
+        angle_history_list = angle_history_list[:MEM_SIZE]
+
+    angle_history_array = np.array(angle_history_list, dtype=np.float32).reshape(1, MEM_SIZE)
+
+    angle = predict_angle(drive_model, drive_image, angle_history_array)
+
+    angle_history.append(angle)
+
     return angle, SPEED, {}
 
 
 # ---------------- Inference ----------------
 
-def predict_angle(drive_model: CompiledModel, img: ndarray) -> float:
+def predict_angle(drive_model: CompiledModel, img: ndarray, angle_history: ndarray) -> float:
     """Run drive model inference on an image"""
-    out = drive_model(img)[0]
+    result = drive_model([img, angle_history])
+    out = result[0]
     return float(np.array(out).ravel()[0])
 
 
@@ -60,3 +83,4 @@ def img_to_tensor(img: Image.Image) -> tuple[ndarray, ndarray]:
     drive_batched = np.transpose(drive_arr, (2, 0, 1))[None, ...]  # [1, 3, 84, 160]
 
     return drive_batched, sign_batched
+ 
